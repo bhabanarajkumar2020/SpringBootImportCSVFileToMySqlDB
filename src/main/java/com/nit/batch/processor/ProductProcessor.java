1fed5605558f55948f5ba72606d6af57ba57d83a
Admin <Admin@DESKTOP-PN1T6AU>
2020-08-28 13:12:59 +0530

    MS-6764-PROCESSOR UPDATED WITH LOGS MSG

--- src/main/java/com/nit/batch/processor/ProductProcessor.java
+++ src/main/java/com/nit/batch/processor/ProductProcessor.java
@@ -1,20 +1,30 @@
 package com.nit.batch.processor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 
 import com.nit.batch.model.Product;
 
 public class ProductProcessor implements ItemProcessor<Product, Product> {
 
+	private static final Logger log = LoggerFactory.getLogger(ProductProcessor.class);
+
 	@Override
 	public Product process(Product item) throws Exception {
-		// Perform Validation
-		var prodCost = item.getProdCost();
-		var discount = prodCost * 25 / 100.0;
-		var gst = prodCost * 18 / 100.0;
-		// Set the discount and gst
-		item.setProdDisc(discount);
-		item.setProdGst(gst);
+		try {
+			log.info("STARTED");
+			// Perform Validation
+			var prodCost = item.getProdCost();
+			var discount = prodCost * 25 / 100.0;
+			var gst = prodCost * 18 / 100.0;
+			// Set the discount and gst
+			item.setProdDisc(discount);
+			item.setProdGst(gst);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		log.info("ENDED");
 		return item;
-	}      
+	}
 }
