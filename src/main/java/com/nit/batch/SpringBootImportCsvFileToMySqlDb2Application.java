daf331b4ae074f82b9d36990bc8aa358e5558d00
Admin <Admin@DESKTOP-PN1T6AU>
2020-08-28 10:46:09 +0530

    WS-7867-New File commit

--- src/main/java/com/nit/batch/SpringBootImportCsvFileToMySqlDb2Application.java
+++ src/main/java/com/nit/batch/SpringBootImportCsvFileToMySqlDb2Application.java
@@ -8,6 +8,7 @@ public class SpringBootImportCsvFileToMySqlDb2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootImportCsvFileToMySqlDb2Application.class, args);
+		System.out.println("Started application");
 	}
 
 }
